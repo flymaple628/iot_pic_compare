cdc08c84fe5ada75f4df68488a828f423ed4d868
Aaron Chen <flymaple628@gmail.com>
2015-08-22 20:22:28 +0800

    add test index

--- app/controllers/test_datas_controller.rb
+++ app/controllers/test_datas_controller.rb
@@ -1,10 +1,16 @@
 class TestDatasController < ApiController
 
+	def index
+		test_data=Test.all
+
+		render :json=>test_data.to_json
+	end
+
 	def show
 		data = params[:id]
 
 		test_data=Test.create(:test_data=>data)
-
+		redirect_to test_datas_path
 	end
 
 
